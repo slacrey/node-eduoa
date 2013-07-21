c9331f348e73036cfd86d5da23c5cdc57791c3f5
linfeng <scstlinfeng@yahoo.cn>
2013-07-21 23:46:36 +0800

    初始化项目

--- src/main/java/com/node/eduoa/entity/OaRegistrationAttendance.java
+++ src/main/java/com/node/eduoa/entity/OaRegistrationAttendance.java
@@ -2,6 +2,9 @@ package com.node.eduoa.entity;
 
 import com.node.system.entity.IdEntity;
 
+import javax.persistence.Column;
+import java.util.Date;
+
 /**
  *
  * User: linfeng
@@ -10,8 +13,136 @@ import com.node.system.entity.IdEntity;
  * To change this template use File | Settings | File Templates.
  */
 public class OaRegistrationAttendance extends IdEntity {
+
     private static final long serialVersionUID = 7116375295309848920L;
 
+    @Column(name = "attendance_date")
+    private Date attendanceDate;//考勤日期
+    @Column(name = "teacher_id")
+    private Long teacherId;
+    @Column(name = "teacher_name")
+    private String teacherName;
 
+    @Column(name = "organization_id")
+    private Long organizationId;
 
+    @Column(name = "organization_name")
+    private String organizationName;
+
+    @Column(name = "morning_start")
+    private Integer morningStart;
+
+    @Column(name = "morning_end")
+    private Integer morningEnd;
+
+    @Column(name = "afternoon_start")
+    private Integer afternoonStart;
+
+    @Column(name = "afternoon_end")
+    private Integer afternoonEnd;
+
+    @Column(name = "night_start")
+    private Integer nightStart;
+
+    @Column(name = "night_end")
+    private Integer nightEnd;
+
+    @Column(name = "create_time")
+    private Date createTime;
+
+    public Date getAttendanceDate() {
+        return attendanceDate;
+    }
+
+    public void setAttendanceDate(Date attendanceDate) {
+        this.attendanceDate = attendanceDate;
+    }
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public Long getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(Long organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    public Integer getMorningStart() {
+        return morningStart;
+    }
+
+    public void setMorningStart(Integer morningStart) {
+        this.morningStart = morningStart;
+    }
+
+    public Integer getMorningEnd() {
+        return morningEnd;
+    }
+
+    public void setMorningEnd(Integer morningEnd) {
+        this.morningEnd = morningEnd;
+    }
+
+    public Integer getAfternoonStart() {
+        return afternoonStart;
+    }
+
+    public void setAfternoonStart(Integer afternoonStart) {
+        this.afternoonStart = afternoonStart;
+    }
+
+    public Integer getAfternoonEnd() {
+        return afternoonEnd;
+    }
+
+    public void setAfternoonEnd(Integer afternoonEnd) {
+        this.afternoonEnd = afternoonEnd;
+    }
+
+    public Integer getNightStart() {
+        return nightStart;
+    }
+
+    public void setNightStart(Integer nightStart) {
+        this.nightStart = nightStart;
+    }
+
+    public Integer getNightEnd() {
+        return nightEnd;
+    }
+
+    public void setNightEnd(Integer nightEnd) {
+        this.nightEnd = nightEnd;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
 }
---
+++ src/main/java/com/node/eduoa/utils/WeekUtils.java
@@ -0,0 +1,88 @@
+package com.node.eduoa.utils;
+
+import java.text.DateFormat;
+import java.util.*;
+
+/**
+ * User: linfeng at Administrator
+ * Date: 13-7-21
+ * Time: 下午6:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class WeekUtils {
+
+    // 用来全局控制 上一周，本周，下一周的周数变化
+    private static int weeks = 0;
+
+    // 获得当前日期与本周一相差的天数
+    private int getMondayPlus() {
+        Calendar cd = Calendar.getInstance();
+        // 获得今天是一周的第几天，星期日是第一天，星期二是第二天......
+        int dayOfWeek = cd.get(Calendar.DAY_OF_WEEK);
+        if (dayOfWeek == 1) {
+            return -6;
+        } else {
+            return 2 - dayOfWeek;
+        }
+    }
+
+    // 获得上周星期一的日期
+    public String getPreviousMonday() {
+        weeks--;
+        int mondayPlus = this.getMondayPlus();
+        GregorianCalendar currentDate = new GregorianCalendar();
+        currentDate.add(GregorianCalendar.DATE, mondayPlus + 7 * weeks);
+        Date monday = currentDate.getTime();
+        DateFormat df = DateFormat.getDateInstance();
+        String preMonday = df.format(monday);
+        return preMonday;
+    }
+
+    // 获得本周星期一的日期
+    public String getCurrentMonday() {
+        weeks = 0;
+        int mondayPlus = this.getMondayPlus();
+        GregorianCalendar currentDate = new GregorianCalendar();
+        currentDate.add(GregorianCalendar.DATE, mondayPlus);
+        Date monday = currentDate.getTime();
+        DateFormat df = DateFormat.getDateInstance();
+        String preMonday = df.format(monday);
+        return preMonday;
+    }
+
+    // 获得下周星期一的日期
+    public String getNextMonday() {
+        weeks++;
+        int mondayPlus = this.getMondayPlus();
+        GregorianCalendar currentDate = new GregorianCalendar();
+        currentDate.add(GregorianCalendar.DATE, mondayPlus + 7 * weeks);
+        Date monday = currentDate.getTime();
+        DateFormat df = DateFormat.getDateInstance();
+        String preMonday = df.format(monday);
+        return preMonday;
+    }
+
+    // 获得相应周的周日的日期
+    public String getSunday() {
+        int mondayPlus = this.getMondayPlus();
+        GregorianCalendar currentDate = new GregorianCalendar();
+        currentDate.add(GregorianCalendar.DATE, mondayPlus + 7 * weeks + 6);
+        Date monday = currentDate.getTime();
+        DateFormat df = DateFormat.getDateInstance();
+        String preMonday = df.format(monday);
+        return preMonday;
+    }
+
+    public List<Date> getCurrentDateOfWeek() {
+        List<Date> resultDate = new ArrayList<Date>(7);
+        for (int i=0; i<= 6; i++) {
+            int mondayPlus = this.getMondayPlus();
+            GregorianCalendar currentDate = new GregorianCalendar();
+            currentDate.add(GregorianCalendar.DATE, mondayPlus + i);
+            Date monday = currentDate.getTime();
+            resultDate.add(monday);
+        }
+        return resultDate;
+    }
+
+}
